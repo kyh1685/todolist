4b037deef4dd1106d3d9f58489c966be419f6a6b
KimYunhee <qnvnfls@naver.com>
2022-10-03 23:59:04 +0900

    🎨 create Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+import TodoTemplate  from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </>
   );
 }
 
